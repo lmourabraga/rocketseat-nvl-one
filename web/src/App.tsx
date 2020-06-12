7915082cbdd1417567af33f7012b9ceb55bce4c1
Luã Braga <ldemourabraga@gmail.com>
2020-06-11 21:28:52 -0300

    Add component with props and state

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import Header from './Header';
+
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <h1>Hello world!</h1>
+    <div>
+      <Header title="Hello world" />
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+    </div>
   );
 }
 
