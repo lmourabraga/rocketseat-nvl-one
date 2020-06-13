51dec560a0cf49c18f362499e494f4fca5b8d25a
Luã Braga <ldemourabraga@gmail.com>
2020-06-13 17:15:31 -0300

    Add homepage and routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Routes from './routes';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header title="Hello world" />
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
-    </div>
+    <Routes />
   );
 }
 
